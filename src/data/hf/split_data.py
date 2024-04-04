ce424386afcedacebcf00cb4c5bd9d15efab74b3
tiginamaria <tiginamaria@gmail.com>
2024-04-04 15:17:38 +0200

    Fix data upload

--- src/data/hf/split_data.py
+++ src/data/hf/split_data.py
@@ -1,19 +1,21 @@
 import datasets
 import hydra
+from dotenv import load_dotenv
 from omegaconf import DictConfig
 
 from src.utils.hf_utils import update_hf_data_splits
 
 
-def split_data(df: datasets.Dataset, split: str, test_data_ids: list[int]):
+def split_data(df: datasets.Dataset, split: str, test_data_ids: list[str]):
+    test_data_ids = [i.lower() for i in test_data_ids]
     if split == 'dev':
         return df
 
     if split == 'test':
-        return df.filter(lambda dp: dp['id'] in test_data_ids)
+        return df.filter(lambda dp: dp['text_id'] in test_data_ids)
 
     if split == 'train':
-        return df.filter(lambda dp: dp['id'] not in test_data_ids)
+        return df.filter(lambda dp: dp['text_id'] not in test_data_ids)
 
 
 @hydra.main(config_path="../../../configs/data", config_name="server", version_base=None)
@@ -24,4 +26,5 @@ def run_split_data(config: DictConfig):
 
 
 if __name__ == '__main__':
+    load_dotenv()
     run_split_data()
